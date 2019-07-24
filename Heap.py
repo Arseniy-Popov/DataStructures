63c3019b416b486dbc9768bfc04acf05d3605a1e
thatanalyst <ubuntu@ip-172-31-46-1.ec2.internal>
2019-07-24 15:28:22 +0000

    -

--- Heap.py
+++ Heap.py
@@ -1,4 +1,4 @@
-from PriorityQueue import PriorityQueueBase
+from PriorityQueue.PriorityQueue import PriorityQueueBase
 
 
 class Heap(PriorityQueueBase):
@@ -8,41 +8,40 @@ class Heap(PriorityQueueBase):
     def __len__(self):
         return len(self.data)
 
-    def _leftIndex(self, index):
-        return 2 * index + 1
+    def _checkIndex(self, index):
+        if index > len(self) - 1:
+            return None
+        else:
+            return index 
+        
+    def _left(self, index):
+        result = 2 * index + 1
+        return self._checkIndex(result)
+        
+    def _right(self, index):
+        result = 2 * index + 2
+        return self._checkIndex(result)
 
-    def _rightIndex(self, index):
-        return 2 * index + 2
-
-    def _parentIndex(self, index):
+    def _parent(self, index):
         return (index - 1) // 2
 
     def _swap(self, index1, index2):
         self.data[index1], self.data[index2] = self.data[index2], self.data[index1]
 
     def _upHeap(self, index=None):
+        """ Recursively swap items from leaf to root. """
         if index is None:
-            index = len(self)
-        if self.data[self._parentIndex(index)] < self.data[index]:
-            self._swap(self._parentIndex(index), index)
-            self._upHeap(index=self._parentIndex(index))
+            index = len(self) - 1 
+        if self.data[self._parent(index)] > self.data[index] and index != 0:
+            self._swap(self._parent(index), index)
+            self._upHeap(index=self._parent(index))
 
     def _downHeap(self, index=None):
+        """ Recursively swap items from root to leaf. """
         if index is None:
             index = 0
-        if (
-            self.data[self._leftIndex(index)] < self.data[index]
-            and self.data[self._leftIndex(index)] < self.data[self._rightIndex(index)]
-        ):
-            self._swap(self._leftIndex(index), index)
-            self._downHeap(index=self._leftIndex(index))
-        elif (
-            self.data[self._rightIndex(index)] < self.data[index]
-            and self.data[self._leftIndex(index)] > self.data[self._rightIndex(index)]
-        ):
-            self._swap(self._rightIndex(index), index)
-            self._downHeap(index=self._rightIndex(index))
-
+        if 
+        
     # PUBLIC METHODS
 
     def add(self, item, priority):
@@ -52,10 +51,10 @@ class Heap(PriorityQueueBase):
     def min(self):
         if len(self) == 0:
             raise ValueError("empty queue")
-        return self.data[0][0], self.data[0][1]
+        return self.data[0].item, self.data[0].priority
 
     def removeMin(self):
         min = self.min()
-        self.data[0] = self.data[-1]
+        self.data[0] = self.data.pop()
         self._downHeap()
         return min
---
+++ tests.py
@@ -0,0 +1,50 @@
+import unittest
+from Heap import Heap
+from PriorityQueue.tests import test_priotityQueue
+
+
+class test_heap(test_priotityQueue):
+    def build_queue(self):
+        self.queue = Heap()
+
+    def priorities(self, target):
+        result = []
+        for i in target.data:
+            result.append(i.priority)
+        return result
+
+    def test_parent_index(self):
+        self.assertEqual(self.queue._parent(5), 2)
+        self.assertEqual(self.queue._parent(6), 2)
+        self.assertEqual(self.queue._parent(2), 0)
+
+    def test_child_index(self):
+        self.assertEqual(self.queue._left(1), 3)
+        self.assertEqual(self.queue._right(1), 4)
+        self.assertEqual(self.queue._left(6), None)
+
+    def test_priorities(self):
+        print(self.priorities(self.queue))
+
+    def test_adding(self):
+        self.queue2 = Heap()
+        tuples = [
+            ("s", 5),
+            ("r", 2),
+            ("d", 3),
+            ("h", 15),
+            ("q", 1),
+            ("w", 20),
+            ("e", 7),
+        ]
+        for i in tuples:
+            self.queue2.add(i[0], i[1])
+            print(self.priorities(self.queue2))
+
+
+if __name__ == "__main__":
+    tests = []
+    # repeat line for each class
+    tests.append(unittest.TestLoader().loadTestsFromTestCase(test_heap))
+    suite = unittest.TestSuite(tests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
