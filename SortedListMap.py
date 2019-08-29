b0126968a6ec348850285736dbbac9dc09ab1830
Arseniy Popov <arseniypopov@gmail.com>
2019-08-29 07:50:39 +0000

    in progress

--- SortedListMap.py
+++ SortedListMap.py
@@ -6,28 +6,31 @@ class SortedListMap(MapBase):
         self._data = []
     
     def _binarySearch(self, key, low=None, high=None):
+        if len(self) == 0:
+            return (False, None)
         if low is None and high is None:
             low, high = 0, len(self._data) - 1
         if high == low and self._data[low]._key != key:
-            if self._data[low]._key > key:
-                return (False, low)
+            return (False, low)
         mid = (high + low) // 2
         if self._data[mid]._key == key:
             return (True, mid)
         if self._data[mid]._key > key:
-            self._bisectionSearch(key, low=low, high=mid-1)
+            return self._binarySearch(key, low=low, high=mid-1)
         else:
-            self._bisectionSearch(key, low=mid+1, high=high)
+            return self._binarySearch(key, low=mid+1, high=high)
     
     def __setitem__(self, key, value):
         foundKey, index = self._binarySearch(key)
         if foundKey:
             self._data[index]._value == value
-        else:
+        elif not foundKey and index is not None:
             if key < self._data[index]._key:
                 self._data.insert(index, self._Item(key, value))
             else:
                 self._data.insert(index+1, self._Item(key, value))
+        else:
+            self._data.append(self._Item(key, value))
         
     def __getitem__(self, key):
         foundKey, index = self._binarySearch(key)
