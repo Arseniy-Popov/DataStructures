45201450679793cc81afc6d1ab8e65b434049b0b
Arseniy Popov <arseniypopov@gmail.com>
2019-11-21 18:39:40 +0000

    -

--- MapTree/MapTree.py
+++ MapTree/MapTree.py
@@ -3,5 +3,15 @@ from DataStructures.Tree import LinkedBinaryTree
 
 
 class MapTree(MapBase, LinkedBinaryTree):
+    def _findKey(self, key, current, parent=None):
+        if current is None:
+            return parent
+        if current.item._key < key:
+            self._findKey(key, self.right(current), parent=current)
+        elif current.item._key > key:
+            self._findKey(key, self.left(current), parent=current)
+        elif current.item._key == key:
+            return current
+    
     def __setitem__(self, key, value):
         
