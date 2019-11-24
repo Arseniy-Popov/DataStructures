83466a55d51486d28c31e64f40696d5dc8c56719
Arseniy Popov <arseniypopov@gmail.com>
2019-11-24 20:17:15 +0000

    -

--- MapTree/MapTree.py
+++ MapTree/MapTree.py
@@ -4,33 +4,36 @@ from DataStructures.Tree.Tree import LinkedBinaryTree
 
 class MapTree(LinkedBinaryTree, MapBase):
     def _findKey(self, key, current, parent=None):
-        if current is None:
+        if current.item() is None:
             return parent
-        if current.item._key < key:
+        if current.item()._key < key:
             self._findKey(key, self.right(current), parent=current)
-        elif current.item._key > key:
+        elif current.item()._key > key:
             self._findKey(key, self.left(current), parent=current)
-        elif current.item._key == key:
+        elif current.item()._key == key:
             return current
     
     def __getitem__(self, key):
         position = self._findKey(key, self.root())
-        if position is None or position.item._key == key:
-            return position.item._value
+        if position is None or position.item()._key == key:
+            return position.item()._value
         else:
             raise ValueError('key not found')
     
     def __setitem__(self, key, value):
         position = self._findKey(key, self.root())
-        if position is None:
+        if position.item() is None:
             self.addRoot(self._Item(key, value))
-        elif position.item._key != key:
-            if position.item._key < key:
+        elif position.item()._key != key:
+            if position.item()._key < key:
                 self.addRight(position, self._Item(key, value))
             else:
                 self.addLeft(position, self._Item(key, value))
         else:
-            position.item._value = value
+            position.item()._value = value
     
     def __delitem__(self, key):
-        return 
+        pass
+
+    def __iter__(self):
+        yield from self.traverseInorder()
